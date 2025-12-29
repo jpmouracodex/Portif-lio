dabc669674467d4e2fb275b768b6f4bfa20238a2
jpmouracodex <47841392+jpmouracodex@users.noreply.github.com>
2025-12-28 21:08:16 -0300

    add habilidades

--- static/script.js
+++ static/script.js
@@ -10,3 +10,15 @@ menuMobile.addEventListener('click', () => {
 
   body.classList.toggle('menu-nav-active');
 });
+
+
+const navItem = document.querySelectorAll('.nav-item');
+
+navItem.forEach(item => { 
+    item.addEventListener('click', () => {
+        if(body.classList.contains('menu-nav-active')) {
+            body.classList.remove('menu-nav-active');
+            menuMobile.classList.replace('bi-x', 'bi-list');
+        }
+        })
+})
